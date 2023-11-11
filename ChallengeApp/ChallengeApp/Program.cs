52c849f18a382c908c26fb3d75c864363bad6334
ksume <ksumeracki@gmail.com>
2023-11-11 22:00:31 +0000

    zadanie 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,1 +1,23 @@
-﻿Console.WriteLine("Hello, byku jak tam!");
+﻿string name = "Krzysiek";
+var age = 36;
+string gender = "male";
+
+
+
+if(gender =="famale" && age < 30)
+{
+
+    Console.WriteLine("Kobieta ponizej 30 lat");
+}
+else if(name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa lat 33");
+}
+else if(gender == "male" && age < 18)
+{
+    Console.WriteLine("Niepelnoletni mezczyzna");
+}
+else
+{
+    Console.WriteLine("Nie zgodnosc torsamosci");
+}

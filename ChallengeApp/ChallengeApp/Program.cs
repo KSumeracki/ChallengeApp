6cbb55b0584b537eff4028c82c425ac83b1cf5ef
ksume <ksumeracki@gmail.com>
2023-11-12 21:56:46 +0000

    Cyferki

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,23 +1,81 @@
-﻿string name = "Krzysiek";
-var age = 36;
-string gender = "male";
+﻿using System.Diagnostics.Metrics;
 
+int number = 3543;
+string numberInString = number.ToString();
+char[] LETTERS = numberInString.ToCharArray();
 
+int counter0 = 0;
+int counter1 = 0;
+int counter2 = 0;
+int counter3 = 0;
+int counter4 = 0;
+int counter5 = 0;
+int counter6 = 0;
+int counter7 = 0;
+int counter8 = 0;
+int counter9 = 0;
 
-if(gender =="famale" && age < 30)
+foreach (char letter in LETTERS)
 {
+    if (letter == '0')
+    {
+        counter0++;
 
-    Console.WriteLine("Kobieta ponizej 30 lat");
+    }
+    else if (letter == '1')
+    {
+        counter1++;
+
+    }
+    else if (letter == '2')
+    {
+        counter2++;
+
+    }
+    else if (letter == '3')
+    {
+        counter3++;
+
+    }
+    else if (letter == '4')
+    {
+        counter4++;
+
+    }
+    else if (letter == '5')
+    {
+        counter5++;
+
+    }
+    else if (letter == '6')
+    {
+        counter6++;
+
+    }
+    else if (letter == '7')
+    {
+        counter7++;
+
+    }
+    else if (letter == '8')
+    {
+        counter8++;
+
+    }
+    else if (letter == '9')
+    {
+        counter9++;
+
+    }
 }
-else if(name == "Ewa" && age == 33)
-{
-    Console.WriteLine("Ewa lat 33");
-}
-else if(gender == "male" && age < 18)
-{
-    Console.WriteLine("Niepelnoletni mezczyzna");
-}
-else
-{
-    Console.WriteLine("Nie zgodnosc torsamosci");
-}
+Console.WriteLine("Cyferki");
+Console.WriteLine("0=>" + counter0);
+Console.WriteLine("1=>" + counter1);
+Console.WriteLine("2=>" + counter2);
+Console.WriteLine("3=>" + counter3);
+Console.WriteLine("4=>" + counter4);
+Console.WriteLine("5=>" + counter5);
+Console.WriteLine("6=>" + counter6);
+Console.WriteLine("7=>" + counter7);
+Console.WriteLine("8=>" + counter8);
+Console.WriteLine("9=>" + counter9);
